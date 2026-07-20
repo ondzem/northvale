174bb4f819619f5712c634725b34785080a777fe
ondzem <ond.invest@gmail.com>
2026-07-20 12:39:53 +0200

    feat: restore pre-registration gate

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ export const FEATURE_FLAGS = {
   showSlabs: false,
   showTestimonials: false, // Set to true to show "Co o nás říkají" testimonials on homepage
   showNewsletter: true,   // Set to true to show newsletter forms on storefront pages
-  preRegistrationActive: false, // If true, redirects normal users to pre-registration landing page
+  preRegistrationActive: true, // If true, redirects normal users to pre-registration landing page
   showCalendar: false,    // Set to true to enable and show TCG Release Calendar 2026
 };
